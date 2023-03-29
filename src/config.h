42b4fb95d2d7a5043f4fb742e58170c14050e804
r4wk <r4wknet@gmail.com>
2023-03-28 23:38:24 -0230

    Blank config

---
+++ src/config.h
@@ -0,0 +1,39 @@
+// pin set
+#define VOLTAGE_PIN A3
+#define PWM_OUT_PIN 9
+#define SENSOR_POWER_PIN 5
+#define ADC_PIN A2
+#define BUTTON_PIN 8
+#define LED_PIN 6
+
+#define LORAWAN_TX 3
+#define LORAWAN_RX 4
+#define LORAWAN_RST 10
+
+#define DEBUG_OUT_ENABLE 1
+
+#define SOIL_ADC_WATER 550
+#define SOIL_ADC_AIR 660
+#define SOIL_ADC_UNIT ((SOIL_ADC_AIR - SOIL_ADC_WATER) / 100.0)
+
+#define AT_TIMEOUT 3000
+
+#define DEVEUI ""
+#define APPEUI ""
+#define APPKEY ""
+
+#define SLEEP_CYCLE 450
+
+// LAZY FORWARD DECLARATION
+void log_init();
+void log_out(const char *log);
+void read_sensor();
+void lorawan_init();
+int lorawan_join();
+void send_lorawan();
+void low_power();
+bool aht_init();
+void pwn_init();
+void log_out_num(const char *log, int num);
+void sendData(String command, const int timeout);
+int sendData_keyword(String command, const int timeout, String keyword);
